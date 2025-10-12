c2b26ba7ccf767fdb3290ce729a02593a9891af7
Freezer609 <84635230+Freezer609@users.noreply.github.com>
2025-10-12 18:28:39 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -985,95 +985,193 @@ function closeAdminPanel() {
 
 
 
+
+
+
+
 // Draggable Cheat Panel Logic
+
+
 
 let isDragging = false;
 
+
+
 let currentX;
+
+
 
 let currentY;
 
+
+
 let initialX;
+
+
 
 let initialY;
 
+
+
 let xOffset = 0;
+
+
 
 let yOffset = 0;
 
 
 
+
+
+
+
 cheatPanel.addEventListener("mousedown", dragStart);
 
+
+
 cheatPanel.addEventListener("mouseup", dragEnd);
+
+
 
 cheatPanel.addEventListener("mousemove", drag);
 
 
 
+
+
+
+
 function dragStart(e) {
 
+
+
     initialX = e.clientX - xOffset;
+
+
 
     initialY = e.clientY - yOffset;
 
 
 
+
+
+
+
     if (e.target === cheatPanel || e.target.classList.contains('panel-header')) {
+
+
 
         isDragging = true;
 
+
+
     }
 
+
+
 }
+
+
+
+
 
 
 
 function dragEnd(e) {
 
+
+
     isDragging = false;
 
+
+
 }
+
+
+
+
 
 
 
 function drag(e) {
 
+
+
     if (isDragging) {
+
+
 
         e.preventDefault();
 
+
+
         currentX = e.clientX - initialX;
+
+
 
         currentY = e.clientY - initialY;
 
 
 
+
+
+
+
         xOffset = currentX;
+
+
 
         yOffset = currentY;
 
 
 
+
+
+
+
         setTranslate(currentX, currentY, cheatPanel);
+
+
 
     }
 
+
+
 }
+
+
+
+
 
 
 
 function setTranslate(xPos, yPos, el) {
 
+
+
     el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+
+
 
 }
 
 
 
+
+
+
+
 closeCheatPanelBtn.addEventListener('click', () => {
 
+
+
+    console.log("Fermeture du panneau de triche.");
+
+
+
     cheatPanel.style.display = 'none';
+
+
 
 });
 
